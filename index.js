5c1b35068c2ad117e9101fdfb54c3fd6bfff7565
Carter Walsh <cwalsh616@gmail.com>
2021-02-11 22:01:58 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+
+// Detecting click
+
+let numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrums; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(event) {
+
+    let buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+  });
+}
+
+// Detecting key press
+
+document.addEventListener("keypress", function(event) {
+
+  console.log(event);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+// What will happen when a button is clicked or a key is pressed.
+
+function makeSound(key) {
+
+  switch (key) {
+
+    case "w":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+// functions for button animations
+
+function buttonAnimation(currentKey) {
+
+  let activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+}, 100);
+
+
+}
